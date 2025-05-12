25d20067f57f6d42469db79ad8115666c4d65aaf
ranjeetV8 <ranjeetyadav.connect@gmail.com>
2025-05-13 01:19:13 +0530

    updated blogRouter   blog.ts

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -1,10 +1,30 @@
 import { Hono } from "hono";
 
-export const blogRouter = new Hono();
+export const blogRouter = new Hono<{
+   Bindings: {
+    DATABASE_URL : String;
+    JWT_SECRET: String;
+   } 
+}>();
 
 
-blogRouter.post('/', (c) => {
-  return c.text("Hello blog here post!");
+blogRouter.use("/*",(c, next) =>{
+    next();
+})
+blogRouter.post('/', async(c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    await prisma.blog.create({
+        data: {
+            title: body.title,
+            content:body.content,
+            autherID: 1
+        }
+    })
+  
 });
 blogRouter.put('/', (c) => {
   return c.text("Hello post put here!");
