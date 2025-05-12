c4a903c937d8451ec575a6a821cf37578d84a81b
ranjeetV8 <ranjeetyadav.connect@gmail.com>
2025-05-13 00:00:16 +0530

    updated  blog.ts

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -1,3 +1,18 @@
 import { Hono } from "hono";
 
-export const userRouter = new Hono();
+export const blogRouter = new Hono();
+
+
+blogRouter.post('/', (c) => {
+  return c.text("Hello blog here post!");
+});
+blogRouter.put('/', (c) => {
+  return c.text("Hello post put here!");
+});
+blogRouter.get('/', (c) => {
+  return c.text("Hello id!");
+});
+
+blogRouter.get('/bulk', (c) => {
+  return c.text("Hello blog bulk!");
+});
